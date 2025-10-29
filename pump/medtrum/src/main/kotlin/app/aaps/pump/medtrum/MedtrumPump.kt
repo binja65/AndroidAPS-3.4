36e1797afbd6e461421522e51ce16d64ef91c72f
Angus <lh0528@gmail.com>
2025-10-29 21:26:48 +0800

    Add validation and filtering in handleBolusStatusUpdate

--- pump/medtrum/src/main/kotlin/app/aaps/pump/medtrum/MedtrumPump.kt
+++ pump/medtrum/src/main/kotlin/app/aaps/pump/medtrum/MedtrumPump.kt
@@ -402,10 +402,22 @@ class MedtrumPump @Inject constructor(
 
     fun handleBolusStatusUpdate(bolusType: Int, bolusCompleted: Boolean, amountDelivered: Double) {
         aapsLogger.debug(LTag.PUMP, "handleBolusStatusUpdate: bolusType: $bolusType bolusCompleted: $bolusCompleted amountDelivered: $amountDelivered")
-        bolusProgressLastTimeStamp = dateUtil.now()
-        _bolusAmountDelivered.value = amountDelivered
-        BolusProgressData.delivered = amountDelivered
-        bolusDone = bolusCompleted
+        aapsLogger.debug(LTag.PUMP, "handleBolusStatusUpdate: BolusProgressData.insulin=${BolusProgressData.insulin}, old delivered=${BolusProgressData.delivered}, new delivered=$amountDelivered, bolusDone=$bolusDone")
+        
+        val insulin = BolusProgressData.insulin
+        if (insulin > 0) {
+            if (amountDelivered >= 0 && amountDelivered <= insulin && amountDelivered >= BolusProgressData.delivered) {
+                bolusProgressLastTimeStamp = dateUtil.now()
+                _bolusAmountDelivered.value = amountDelivered
+                BolusProgressData.delivered = amountDelivered
+                bolusDone = bolusCompleted
+            } else {
+                aapsLogger.warn(LTag.PUMP, "handleBolusStatusUpdate: Ignoring invalid delivered amount: $amountDelivered (insulin=$insulin, previous delivered=${BolusProgressData.delivered})")
+            }
+        } else {
+            // No active bolus, ignore this update (likely stale data from pump reconnection)
+            aapsLogger.debug(LTag.PUMP, "handleBolusStatusUpdate: Ignoring update - no active bolus (insulin=$insulin)")
+        }
     }
 
     fun handleBasalStatusUpdate(basalType: BasalType, basalValue: Double, basalSequence: Int, basalPatchId: Long, basalStartTime: Long) {
