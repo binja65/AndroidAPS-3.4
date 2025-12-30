9bf1ab7d7973400b24728d67ebc40dc84f7e3d71
bastiaanv <bastiankpn7800@gmail.com>
2025-12-30 19:50:00 +0100

    fix: merge conflicts

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/GlunovoPlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/GlunovoPlugin.kt
@@ -23,6 +23,8 @@ import app.aaps.core.interfaces.resources.ResourceHelper
 import app.aaps.core.interfaces.source.BgSource
 import app.aaps.core.interfaces.utils.DateUtil
 import app.aaps.core.interfaces.utils.fabric.FabricPrivacy
+import app.aaps.core.keys.interfaces.Preferences
+import app.aaps.plugins.source.keys.GlunovoLongKey
 import app.aaps.plugins.source.fragments.BGSourceFragment
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
